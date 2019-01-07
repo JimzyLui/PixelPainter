64c67be7e301ec8d7f41ba0fa9ae7f4f54c71251
Jimzy <JimzyLui@users.noreply.github.com>
2019-01-06 19:29:15 -1000

    MVP completed

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -3,7 +3,7 @@ const pixelPainter = ((iWidth, iHeight) => {
   const leftPanel = document.createElement("div"); // contains colorPalette and buttons
   leftPanel.id = "leftPanel";
   let paintColor = "black";
-  arrColors = [
+  const arrColors = [
     "red",
     "green",
     "blue",
@@ -30,17 +30,6 @@ const pixelPainter = ((iWidth, iHeight) => {
     "beige",
     "gold"
   ];
-  // const canvas = document.createElement("div"); // Box width and height
-  // const colorPalette = document.createElement("div");
-  // const iiSquareWidth = 5;
-  // const iSquareWidth = iiSquareWidth + "px";
-  // canvas.id = "canvas";
-  // canvas.className = "container";
-  // colorPalette.id = "colorPalette";
-  // colorPalette.className = "container";
-
-  // canvas.width = iiSquareWidth * iWidth;
-  // canvas.height = iiSquareWidth * iHeight;
 
   // Padding
   const p = 0;
@@ -65,6 +54,9 @@ const pixelPainter = ((iWidth, iHeight) => {
             box.id = "box" + iBoxCounter.toString();
 
             box.addEventListener("mouseover", changeColor);
+            box.addEventListener("click", changeColor);
+            box.addEventListener("mousedown", onMouseDown);
+            box.addEventListener("mouseup", onMouseUp);
             break;
           case "palette":
             box.style = "background-color:" + arrColors[iBoxCounter];
@@ -82,30 +74,47 @@ const pixelPainter = ((iWidth, iHeight) => {
   }
 
   function createBox(boxClass) {
-    const box = document.createElement("div");
+    const box = document.createElement("span");
     box.classList = boxClass;
     return box;
   }
-  function changeColor(e) {
-    if (bMouseReleased === false) {
-      this.style.backGroundColor = paintColor;
-    }
+  function onMouseDown() {
+    bMouseReleased = false;
   }
-  function setColor(e) {
-    paintColor = this.style.backGroundColor = "blue";
-    document.querySelector("#paintBox").style.backGroundColor = paintColor;
+  function onMouseUp() {
+    bMouseReleased = true;
+  }
+  function changeColor(evt) {
+    if (bMouseReleased === false || evt.type == "click") {
+      this.style.backgroundColor = paintColor;
+    }
+    // this.style.backgroundColor = paintColor;
+  }
+  function setColor() {
+    paintColor = this.style.backgroundColor;
+    paintBox.style.backgroundColor = paintColor;
+    paintBoxLabel.innerHTML = paintColor;
   }
   function toggleMouseClick(e) {
     bMouseClicked = !bMouseClicked;
   }
   function erase() {
     paintColor = "white";
+    paintBox.style.backgroundColor = paintColor;
+    paintBoxLabel.innerHTML = paintColor;
   }
   function clear() {
-    arrCanvasBoxes = document.querySelectorAll(".canvas");
-    arrCanvasBoxes.map(x => (x.style.backGroundColor = "white"));
+    arrCanvasBoxes = document.querySelectorAll(".box.canvas");
+    console.log(arrCanvasBoxes);
+    for (let i = 0; i < arrCanvasBoxes.length; i++) {
+      arrCanvasBoxes[i].style.backgroundColor = "white";
+    }
+    // arrCanvasBoxes.map(obj => {
+    //   obj.style.backgroundColor = "white";
+    //   return obj;
+    // });
   }
-  const canvas = createGrid(iWidth, iHeight, "box", "canvas");
+  const canvas = createGrid(iWidth, iHeight, "box canvas", "canvas");
   const colorPalette = createGrid(5, 5, "box smBox", "palette");
   leftPanel.appendChild(colorPalette);
   const btnErase = document.createElement("button");
@@ -116,12 +125,21 @@ const pixelPainter = ((iWidth, iHeight) => {
   btnClear.addEventListener("click", clear);
   leftPanel.appendChild(btnErase);
   leftPanel.appendChild(btnClear);
-  const colorBox = document.createElement("span");
+  const colorBoxPanel = document.createElement("div");
+  const colorBoxLabel = document.createElement("div");
+  colorBoxLabel.innerHTML = "Paint Color:";
+  const paintBoxRow = document.createElement("div");
+  paintBoxRow.id = "paintBoxRow";
   const paintBox = createBox("box smBox");
+  const paintBoxLabel = document.createElement("label");
+  colorBoxPanel.id = "colorBoxPanel";
+  paintBoxLabel.innerHTML = paintColor;
   paintBox.id = "paintBox";
-  colorBox.appendChild(paintBox);
-  colorBox.innerHTML = "Paint Color";
-  leftPanel.appendChild(colorBox);
+  paintBoxRow.appendChild(paintBoxLabel);
+  paintBoxRow.appendChild(paintBox);
+  colorBoxPanel.appendChild(colorBoxLabel);
+  colorBoxPanel.appendChild(paintBoxRow);
+  leftPanel.appendChild(colorBoxPanel);
   pp.appendChild(leftPanel);
   pp.appendChild(canvas);
-})(15, 20);
+})(80, 40);
